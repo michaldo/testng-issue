2178bffed57d56f1f63e386a4fed147c9ac3a400
Michal Domagala <michal.domagala@syncron.com>
2016-10-19 11:51:11 +0200

    init

---
+++ demo/src/main/java/demo/Demo.java
@@ -0,0 +1,5 @@
+package demo;
+
+public class Demo extends wf.WF {
+
+}
---
+++ demo/src/test/java/demo/NewTest.java
@@ -0,0 +1,10 @@
+package demo;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  new wf.WF();
+  }
+}
